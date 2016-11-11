ac95c5fabcb42bed01cebbe93d6a956ec7fb1b52
stu <stu@itwill>
2016-11-11 14:53:52 +0900

    새로운 기능

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -7,6 +7,7 @@ public class AppMain {
 		System.out.println("Hello, World");
 		System.out.println("안녕하세요.");
         System.out.println("Test 3...");
+        System.out.println("새로운 기능 추가");
 	}
 
 }
