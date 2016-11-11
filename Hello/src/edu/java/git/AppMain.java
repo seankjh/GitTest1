dfafbb92db449864a8fd24196c1f1fc7311d34ea
stu <stu@itwill>
2016-11-11 14:14:06 +0900

    두번째 다시 커밋

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,7 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Git");
-		System.out.println("¾È³çÇÏ¼¼¿ä.");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš”.");
 	}
 
 }
